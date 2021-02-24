d8671f7263aaa7b66d72ef17b50042834f55d3b3
Daksh <dakshjain48@gmail.com>
2021-02-25 05:21:27 +0530

    uibuild

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+//
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { passwordHash: 0, blogs: 0 } )
