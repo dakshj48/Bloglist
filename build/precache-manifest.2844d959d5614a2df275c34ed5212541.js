90b24e3077b43121e6116a33e30f4f28efcb1050
Daksh <dakshjain48@gmail.com>
2021-02-25 16:51:05 +0530

    uibuild

--- build/precache-manifest.2844d959d5614a2df275c34ed5212541.js
+++ build/precache-manifest.2844d959d5614a2df275c34ed5212541.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ae9489f21c34839844044eea7c0799c",
+    "revision": "0c9e03bfa678f061560d5bf2dc6f320c",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.72a3430d.chunk.js"
   },
   {
-    "revision": "58aad7271cb4ad4e8fa5",
-    "url": "/static/js/main.4db93781.chunk.js"
+    "revision": "d3ca7d40ee7b1ef78030",
+    "url": "/static/js/main.5de58633.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7070fa62b37ea25919256b17b4ad0348.js"
+  "/precache-manifest.2844d959d5614a2df275c34ed5212541.js"
 );
 
 self.addEventListener('message', (event) => {
